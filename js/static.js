3863376384524b3d7d5a8ab7fbcdcffa2ed6e64d
minsang <110008434+jms8793@users.noreply.github.com>
2023-07-20 11:14:48 +0900

    questiom 이미지 추가

--- js/static.js
+++ js/static.js
@@ -2,3 +2,8 @@ $(document).ready(function () {
   $("#header").load("../static/header.html");
   $("footer").load("../static/footer.html");
 });
+function go_back() {
+  history.back();
+  let num = localStorage.getItem("pagenumber");
+  localStorage.setItem("pagenumber", Number(num) - 2);
+}
