670d47e9f02f5ac3a2bda46e15186156cbeca5e6
hyeiis <hyeis0807@gmail.com>
2023-07-22 11:56:28 +0900

    Add comment

--- js/static.js
+++ js/static.js
@@ -8,3 +8,12 @@ function go_back() {
   localStorage.setItem("pagenumber", Number(num) - 2);
   sessionStorage.clear();
 }
+
+/*
+$(document).ready(): 문서가 준비되면 매개변수로 넣은 콜백 함수 실행
+
+$().load(): 반환된 데이터를 매치된 요소의 HTML 콘텐츠로 설정
+-> id가 header인 부분에 header.html을 적용시키고,
+footer에 footer.html을 적용
+
+*/
